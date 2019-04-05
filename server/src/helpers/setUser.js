895cf2b4e19ac739844d90a5d026fa4af0262144
Shahid Mawji <shahid.mawji@gmail.com>
2019-04-05 07:47:03 -0400

    Added user address and company info, fixed user update code

--- server/src/helpers/setUser.js
+++ server/src/helpers/setUser.js
@@ -1,7 +1,14 @@
 import fs from 'fs';
 import util from 'util';
+import { getUser } from './';
 const writeFile = util.promisify(fs.writeFile);
 
 export default async function setUser(user) {
-  return writeFile(`./data/users/${user.id}.json`, JSON.stringify(user));
+  try {
+    let currentUser = await getUser(user.id)
+    const newUser = Object.assign(currentUser, user);
+    return writeFile(`./data/users/${newUser.id}.json`, JSON.stringify(newUser));
+  } catch {
+    return Promise.reject("Unable to find or update user")
+  }
 }
